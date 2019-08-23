ba095be0b68b84b389754df873b7563f9b31e6a3
Igor-Tschoeke <igorgabrieltsc@github.com>
2019-08-23 08:29:13 -0300

    Desenvolvido

---
+++ Repository/Estrutura.sql
@@ -0,0 +1,12 @@
+﻿DROP TABLE IF EXISTS pessoas;
+
+CREATE TABLE pessoas (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(100),
+	sobrenome VARCHAR(100),
+	registro_ativo BIT
+
+);
+
+INSERT INTO pessoas(nome, sobrenome, registro_ativo) VALUES
+('Pamilla', 'Silva', 1);
